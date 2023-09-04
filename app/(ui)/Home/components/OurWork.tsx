4762183c4ac4cefffac2bae1fec91bc29f480f92
salahbm <salahbm.001@gmail.com>
2023-09-04 21:53:16 +0900

    ourwork

--- app/(ui)/Home/components/OurWork.tsx
+++ app/(ui)/Home/components/OurWork.tsx
@@ -20,13 +20,24 @@ const ourWork = [
 
 const OurWork = () => {
   return (
-    // <div>
-    <div className={classes.gallery}>
-      {ourWork.map((work, index) => (
-        <div className={`${classes.galleryItem} cursor-move`} key={index}>
-          <Image className={classes.galleryImg} src={work.img} alt="img" />
-        </div>
-      ))}
+    <div>
+      <div className="text-white  flex items-center justify-center  mb-6">
+        <h5 className="italic font-extrabold lg:text-8xl tracking-[0.2em] md:text-8xl text-4xl  ">
+          Our Work in Process
+        </h5>
+
+        <p className=" lg:text-xl italic text-yellow-500   text-xs ">
+          Dive into luxury with our new jewelry collection, where timeless
+          elegance meets modern allure. Elevate your style today!
+        </p>
+      </div>
+      <div className={classes.gallery}>
+        {ourWork.map((work, index) => (
+          <div className={`${classes.galleryItem} cursor-move`} key={index}>
+            <Image className={classes.galleryImg} src={work.img} alt="img" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
