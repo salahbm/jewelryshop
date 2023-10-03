f87ca81742c04497c5e31dfa17f9cde8a9679223
Mukhammad Bakhriddinov <salahbm.001@gmail.com>
2023-10-03 14:31:49 +0900

    card-lib

--- app/(ui)/Cart/components/CreditCardFormat.tsx
+++ app/(ui)/Cart/components/CreditCardFormat.tsx
@@ -1,106 +1,109 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Cards from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
 import styles from "./cart.module.css";
-const CreditCardForm: React.FC = () => {
-  const [cardNumber, setCardNumber] = useState<string[]>(["", "", "", ""]);
-  const [cardHolder, setCardHolder] = useState<string>("");
-  const [expirationMonth, setExpirationMonth] = useState<string>("");
-  const [expirationYear, setExpirationYear] = useState<string>("");
-  const [ccv, setCcv] = useState<string>("");
+const PaymentForm = () => {
+  const [formData, setFormData] = useState({
+    cvc: "",
+    expiry: "",
+    focus: "",
+    name: "",
+    number: "",
+  });
 
-  const handleCardNumberChange = (index: number, value: string) => {
-    const newCardNumber = [...cardNumber];
-    newCardNumber[index] = value;
-    setCardNumber(newCardNumber);
-
-    if (value.length === 4 && index < 3) {
-      const nextInput = document.getElementById(`card-number-${index + 1}`);
-      if (nextInput) {
-        nextInput.focus();
-      }
-    }
-  };
-
-  const handleCardHolderChange = (value: string) => {
-    setCardHolder(value);
-  };
-
-  const handleExpirationMonthChange = (value: string) => {
-    setExpirationMonth(value);
-  };
-
-  const handleExpirationYearChange = (value: string) => {
-    setExpirationYear(value);
-  };
-
-  const handleCcvChange = (value: string) => {
-    setCcv(value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
   return (
-    <div className={styles.checkout}>
-      <div className={styles["credit-card-box"]}>
-        <div className={styles.flip}>
-          <div className={styles.front}>
-            <div className={styles.chip}></div>
-            <div className={styles.logo}>
-              <svg
-                version="1.1"
-                id="visa"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                width="47.834px"
-                height="47.834px"
-                viewBox="0 0 47.834 47.834"
-              >
-                {/* ... SVG Path Data ... */}
-              </svg>
-            </div>
-            <div className={styles.number}>
-              {cardNumber.map((num, index) => (
-                <span key={index}>{num}</span>
-              ))}
-            </div>
-            {/* ... Rest of the front side of the card ... */}
+    <div className=" rounded-lg">
+      <div className={styles.creditCardForm}>
+        <Cards
+          preview={true}
+          cvc={formData.cvc}
+          expiry={formData.expiry}
+          focused={formData.focus}
+          name={formData.name}
+          number={formData.number}
+        />
+
+        <form>
+          <div className={styles.formGroup}>
+            <label htmlFor="number" className={styles.label}>
+              Card Number:
+            </label>
+            <input
+              type="tel"
+              name="number"
+              id="number"
+              placeholder="Card Number"
+              value={formData.number}
+              onChange={handleInputChange}
+              onFocus={(e) =>
+                setFormData({ ...formData, focus: e.target.name })
+              }
+              className={styles.cardInput}
+            />
           </div>
-          <div className={styles.back}>
-            {/* ... Back side of the card ... */}
-          </div>
-        </div>
-      </div>
-      <form className={styles.form} autoComplete="off" noValidate>
-        <fieldset>
-          <label htmlFor="card-number">Card Number</label>
-          {cardNumber.map((_, index) => (
+          <div className={styles.formGroup}>
+            <label htmlFor="name" className={styles.label}>
+              Name:
+            </label>
             <input
               type="text"
-              id={`card-number-${index}`}
-              className={styles["input-cart-number"]}
-              maxLength={4}
-              key={index}
-              value={cardNumber[index]}
-              onChange={(e) => handleCardNumberChange(index, e.target.value)}
+              name="name"
+              id="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleInputChange}
+              onFocus={(e) =>
+                setFormData({ ...formData, focus: e.target.name })
+              }
+              className={styles.cardInput}
             />
-          ))}
-        </fieldset>
-        <fieldset>
-          <label htmlFor="card-holder">Card holder</label>
-          <input
-            type="text"
-            id="card-holder"
-            className={styles["input-card-holder"]}
-            value={cardHolder}
-            onChange={(e) => handleCardHolderChange(e.target.value)}
-          />
-        </fieldset>
-        {/* ... Rest of the form fields ... */}
-        <button className={styles.btn}>
-          <i className="fa fa-lock"></i> submit
-        </button>
-      </form>
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="expiry" className={styles.label}>
+              MM/YY Expiry:
+            </label>
+            <input
+              type="tel"
+              name="expiry"
+              id="expiry"
+              placeholder="MM/YY"
+              value={formData.expiry}
+              onChange={handleInputChange}
+              onFocus={(e) =>
+                setFormData({ ...formData, focus: e.target.name })
+              }
+              className={styles.cardInput}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="cvc" className={styles.label}>
+              CVC:
+            </label>
+            <input
+              type="tel"
+              name="cvc"
+              id="cvc"
+              placeholder="CVC"
+              value={formData.cvc}
+              onChange={handleInputChange}
+              onFocus={(e) =>
+                setFormData({ ...formData, focus: e.target.name })
+              }
+              className={styles.cardInput}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default CreditCardForm;
+export default PaymentForm;
--- app/(ui)/Cart/page.tsx
+++ app/(ui)/Cart/page.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/app/redux/shopSlice";
 import { StoreItem } from "@/app/type";
 import CreditCardForm from "./components/CreditCardFormat";
+import Image from "next/image";
 const CartPage = () => {
   const dispatch = useDispatch();
   const productData = useSelector((state: any) => state.shop.productData);
@@ -23,92 +24,95 @@ const CartPage = () => {
   }
 
   return (
-    <div className=" w-full p-2">
-      <div className=" my-5 flex flex-row items-center gap-4 w-1/2 whitespace-nowrap">
-        <p className="rounded-md border border-gray-500 bg-yellow-300  px-2 ">
-          Total: {productData.length}
-        </p>
-        <button
-          onClick={() => dispatch(resetCart())}
-          className="flex flex-row items-center  justify-center rounded-md border border-gray-500 bg-red-400 px-2"
-        >
-          <h1>Clear</h1>
-          <IoMdClose size={20} />
-        </button>
-      </div>
+    <div className=" w-full p-3 min-h-screen">
       {productData.length > 0 ? (
-        <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 ">
-          {productData.map((product: StoreItem, index: number) => (
-            <div
-              key={index}
-              className="relative my-5 border-2 border-white rounded-lg p-1 min-h-[160px] md:min-h-[200px] "
+        <div>
+          <div className=" my-5 flex flex-row items-center gap-4 w-1/2 whitespace-nowrap">
+            <p className="rounded-md border border-gray-500 bg-yellow-300  px-2 ">
+              Total: {productData.length}
+            </p>
+            <button
+              onClick={() => dispatch(resetCart())}
+              className="flex flex-row items-center  justify-center rounded-md border border-gray-500 bg-red-400 px-2"
             >
-              <p className="text-white text-center text-xl  md:text-2xl whitespace-nowrap">
-                {product.title}
-              </p>
-              <div className=" flex flex-row justify-between py-2">
-                <div className=" w-2/3">
-                  <img
-                    src={product.image}
-                    alt="Product image"
-                    className=" w-full h-full object-contain rounded-lg my-1"
-                  />
-                </div>
-
-                <div className="w-1/3 flex flex-col justify-between gap-5 items-center">
-                  <div className="flex flex-row items-center justify-end  text-xl md:text-3xl ">
-                    <button
-                      onClick={() => dispatch(plusQuantity(product))}
-                      className="text-lime-500 "
-                    >
-                      <CiCirclePlus />
-                    </button>
-
-                    <span className="mx-2 text-yellow-100">
-                      {product.quantity}
-                    </span>
-                    <button
-                      onClick={() => dispatch(minusQuantity(product))}
-                      className=" text-orange-700"
-                    >
-                      <CiCircleMinus />
-                    </button>
-                  </div>
-                  <p className="text-center text-yellow-50">
-                    {product.description}
+              <h1>Clear</h1>
+              <IoMdClose size={20} />
+            </button>
+          </div>
+          <div className="lg:flex flex-row justify-between">
+            <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 lg:w-3/4 ">
+              {productData.map((product: StoreItem, index: number) => (
+                <div
+                  key={index}
+                  className="relative my-5 border-2 border-white rounded-lg p-1 min-h-[160px] md:min-h-[200px] "
+                >
+                  <p className="text-white text-center text-xl  md:text-2xl whitespace-nowrap">
+                    {product.title}
                   </p>
-                  <div className=" flex flex-col items-center  text-white">
-                    <span className="">Each: ${product.price}</span>
-                    <p className="">Total: ${eachTotalPrice(product)}</p>
+                  <div className=" flex flex-row justify-between py-2">
+                    <div className=" w-2/3">
+                      <Image
+                        src={product.image}
+                        alt="Product image"
+                        className=" w-full h-full object-contain rounded-lg my-1"
+                        width={400}
+                        height={400}
+                      />
+                    </div>
+
+                    <div className="w-1/3 flex flex-col justify-between gap-5 items-center">
+                      <div className="flex flex-row items-center justify-end  text-xl md:text-3xl ">
+                        <button
+                          onClick={() => dispatch(plusQuantity(product))}
+                          className="text-lime-500 "
+                        >
+                          <CiCirclePlus />
+                        </button>
+
+                        <span className="mx-2 text-yellow-100">
+                          {product.quantity}
+                        </span>
+                        <button
+                          onClick={() => dispatch(minusQuantity(product))}
+                          className=" text-orange-700"
+                        >
+                          <CiCircleMinus />
+                        </button>
+                      </div>
+                      <p className="text-center text-yellow-50">
+                        {product.description}
+                      </p>
+                      <div className=" flex flex-col items-center  text-white">
+                        <span className="">Each: ${product.price}</span>
+                        <p className="">Total: ${eachTotalPrice(product)}</p>
+                      </div>
+                    </div>
                   </div>
+                  <button
+                    onClick={() => dispatch(deleteItem(product._id))}
+                    className="text-white absolute top-2 right-1 rounded-full border-2 hover:bg-white hover:text-Red text-lg md:text-2xl"
+                  >
+                    <IoMdClose />
+                  </button>
                 </div>
-              </div>
-              <button
-                onClick={() => dispatch(deleteItem(product._id))}
-                className="text-white absolute top-2 right-1 rounded-full border-2 hover:bg-white hover:text-Red text-lg md:text-2xl"
-              >
-                <IoMdClose />
-              </button>
+              ))}
             </div>
-          ))}
+
+            <CreditCardForm />
+          </div>
         </div>
       ) : (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
+        <div className="flex items-center  text-white flex-col justify-between gap-14">
           <HiOutlineShoppingCart size={100} />
-          <p className="">It is empty</p>
+          <p className="text-3xl">It is empty</p>
 
-          <Link href="/Shop">
-            <button className="">Shop more</button>
+          <Link href="/Shop" className="text-3xl">
+            <button className="bg-white hover:bg-yellow-300 text-Red font-bold py-2 px-4 rounded">
+              Shop more
+            </button>
           </Link>
         </div>
       )}
-      <CreditCardForm />
     </div>
   );
 };
