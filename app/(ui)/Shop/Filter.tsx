ed1a273cb3e20104c6a04d9a5017be6011dd2873
Mukhammad Bakhriddinov <salahbm.001@gmail.com>
2023-10-21 11:57:19 +0900

    Filter: logic

--- app/(ui)/Shop/Filter.tsx
+++ app/(ui)/Shop/Filter.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ProductData } from "@/app/constant/productData";
 import {
   Checkbox,
   CssBaseline,
@@ -13,58 +14,86 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import { useState } from "react";
-import { AiFillFilter } from "react-icons/ai";
+import { AiFillFilter, AiOutlineClear } from "react-icons/ai";
 interface FilterDrawerType {
   open: boolean;
   toggleDrawer: () => void;
 }
 const FilterDrawer = ({ open, toggleDrawer }: FilterDrawerType) => {
-  const [costFilters, setCostFilters] = useState({});
-  const [sizeFilter, setSizeFilter] = useState("");
-  const [yearFilter, setYearFilter] = useState("");
-  const [rankingFilter, setRankingFilter] = useState("");
-  const [materialFilter, setMaterialFilter] = useState("");
-  const [goldFilter, setGoldFilter] = useState(false);
-  const [silverFilter, setSilverFilter] = useState(false);
-  const [otherFilter, setOtherFilter] = useState(false);
-  const [typeFilters, setTypeFilters] = useState({
-    ring: false,
-    bracelet: false,
-    other: false,
+  const [filter, setFilter] = useState({
+    cost: 500,
+    category: "All",
+    stone: "All",
+    ranking: 3,
+    material: "All",
+    made: "All",
+    gender: "both",
   });
-  const [handmadeFilter, setHandmadeFilter] = useState(false);
-  const [genderFilter, setGenderFilter] = useState("");
+  const resetData = {
+    cost: 500,
+    category: "All",
+    stone: "All",
+    ranking: 3,
+    material: "All",
+    made: "All",
+    gender: "both",
+  };
+  // Filter function
+  const filterData = () => {
+    const filteredData = ProductData.filter((item: any) => {
+      return (
+        item.cost <= filter.cost &&
+        (filter.category === "All" || item.category === filter.category) &&
+        (filter.stone === "All" || item.stone === filter.stone) &&
+        item.ranking >= filter.ranking &&
+        (filter.material === "All" || item.material === filter.material) &&
+        (filter.made === "All" || item.made === filter.made) &&
+        (filter.gender === "both" || item.gender === filter.gender)
+      );
+    });
 
-  const handleCostChange = (newValue: string) => {
-    setCostFilters(newValue);
+    console.log(filteredData);
+    console.log(filter);
   };
 
-  const handleMetalCheck = (event: any) => {
-    const metal = event.target.name;
-    switch (metal) {
-      case "gold":
-        setGoldFilter(event.target.checked);
-        break;
-      case "silver":
-        setSilverFilter(event.target.checked);
-        break;
-      case "other":
-        setOtherFilter(event.target.checked);
-        break;
-      default:
-        break;
-    }
+  // Event handlers for filter changes
+  const handleCostChange = (event: any, newValue: number) => {
+    setFilter({ ...filter, cost: newValue });
   };
+
+  const handleCategoryChange = (event) => {
+    setFilter({ ...filter, category: event.target.value });
+  };
+
+  const handleStoneChange = (event: any) => {
+    setFilter({ ...filter, stone: event.target.value });
+  };
+
+  const handleRankingChange = (event: any, newValue: number) => {
+    setFilter({ ...filter, ranking: newValue });
+  };
+
+  const handleMaterialChange = (event) => {
+    setFilter({ ...filter, material: event.target.value });
+  };
+
+  const handleMadeChange = (event) => {
+    setFilter({ ...filter, made: event.target.value });
+  };
+
+  const handleGenderChange = (event) => {
+    setFilter({ ...filter, gender: event.target.value });
+  };
+
   const sliderCss = {
     width: useMediaQuery("(max-width: 600px)") ? "50%" : "25%",
     backgroundColor: "#F8F0E3",
-    padding: 4,
+    padding: 2,
     margin: 2,
     borderRadius: 5,
+    overflowX: "hidden",
   };
-  const valuetext = (value: number) => {
-    return `$${value}`;
-  };
+
   const marks = [
     {
       value: 100,
@@ -90,17 +119,35 @@ const FilterDrawer = ({ open, toggleDrawer }: FilterDrawerType) => {
   ];
   return (
     <Drawer open={open} anchor="right" PaperProps={{ sx: sliderCss }}>
-      <button
-        className="text-Red outline hover:bg-red-100 rounded-lg self-center px-5 outline-1 flex flex-row items-center gap-1  text-xl"
-        onClick={toggleDrawer}
-      >
+      <Typography variant="h5" color={"gray"} fontWeight={"bold"}>
         Filter
-        <span>
-          <AiFillFilter />
-        </span>
-      </button>
+      </Typography>
+      <div className="flex md:flex-row justify-around mb-2 mt-2 flex-col gap-1">
+        <button
+          className="text-lime-600 outline hover:bg-red-100 rounded-lg self-center px-5 outline-1 flex flex-row items-center gap-1  text-md"
+          onClick={() => {
+            filterData();
+            toggleDrawer();
+          }}
+        >
+          Filter
+          <span>
+            <AiFillFilter />
+          </span>
+        </button>
+        <button
+          className="text-Red outline hover:bg-red-100 rounded-lg self-center px-5 outline-1 flex flex-row items-center gap-1  text-md"
+          onClick={() => setFilter(resetData)}
+        >
+          Clear
+          <span>
+            <AiOutlineClear />
+          </span>
+        </button>
+      </div>
 
-      <Typography variant="h6" fontWeight={"bold"} color={"#900000"}>
+      <div className="border-b-2 border-[#D9D9D9] m-1" />
+      <Typography variant="h6" fontWeight="bold" color="#900000">
         Cost
       </Typography>
 
@@ -108,166 +155,195 @@ const FilterDrawer = ({ open, toggleDrawer }: FilterDrawerType) => {
         step={50}
         color="error"
         marks={marks}
-        defaultValue={500}
+        value={filter.cost}
         valueLabelDisplay="on"
-        getAriaValueText={valuetext}
-        aria-labelledby="label-always-visible-slider"
+        onChange={handleCostChange}
         max={10000}
         min={100}
       />
-      <div className=" border-b-2 border-[#D9D9D9] m-1" />
-      <Typography variant="h6" fontWeight={"bold"} color={"#900000"}>
-        Type
+
+      <div className="border-b-2 border-[#D9D9D9] m-1" />
+
+      <Typography variant="h6" fontWeight="bold" color="#900000">
+        Category
       </Typography>
 
-      <div>
+      <RadioGroup
+        aria-label="category"
+        name="category"
+        value={filter.category}
+        onChange={handleCategoryChange}
+      >
         <FormControlLabel
-          value="primary"
+          value="Ring"
           control={<Radio color="error" />}
           label="Ring"
         />
         <FormControlLabel
-          value="secondary"
+          value="Bracelet"
           control={<Radio color="error" />}
           label="Bracelet"
         />
         <FormControlLabel
-          value="success"
-          label="Chain"
+          value="Chain"
           control={<Radio color="error" />}
+          label="Chain"
         />
         <FormControlLabel
-          value="success"
-          label="Wedding Ring"
+          value="Earring"
           control={<Radio color="error" />}
+          label="Earring"
         />
-        <FormControlLabel value="error" label="All" control={<Radio />} />
-      </div>
-      <div className=" border-b-2 border-[#D9D9D9] m-1" />
-      <Typography variant="h6" fontWeight={"bold"} color={"#900000"}>
+        <FormControlLabel
+          value="Wedding Ring"
+          control={<Radio color="error" />}
+          label="Wedding Ring"
+        />
+        <FormControlLabel value="All" control={<Radio />} label="All" />
+      </RadioGroup>
+      <div className="border-b-2 border-[#D9D9D9] m-1" />
+
+      <Typography variant="h6" fontWeight="bold" color="#900000">
         Stone
       </Typography>
 
-      <div>
+      <RadioGroup
+        aria-label="stone"
+        name="stone"
+        value={filter.stone}
+        onChange={handleStoneChange}
+      >
         <FormControlLabel
-          value="primary"
+          value="Diamonds"
           control={<Radio color="error" />}
           label="Diamonds"
         />
         <FormControlLabel
-          value="secondary"
+          value="Gem Stone"
           control={<Radio color="error" />}
           label="Gem Stone"
         />
         <FormControlLabel
-          value="success"
+          value="Chain"
+          control={<Radio color="error" />}
           label="Chain"
-          control={<Radio color="error" />}
         />
         <FormControlLabel
-          value="success"
+          value="No Gem"
+          control={<Radio color="error" />}
           label="No Gem"
-          control={<Radio color="error" />}
         />
         <FormControlLabel
-          value="success"
-          label="Pearls"
+          value="Pearls"
           control={<Radio color="error" />}
+          label="Pearls"
         />
-        <FormControlLabel value="error" label="All" control={<Radio />} />
-      </div>
-      <div className=" border-b-2 border-[#D9D9D9] m-1" />
+        <FormControlLabel value="All" control={<Radio />} label="All" />
+      </RadioGroup>
+      <div className="border-b-2 border-[#D9D9D9] m-1" />
 
-      <Typography variant="h6" fontWeight={"bold"} color={"#900000"}>
+      <Typography variant="h6" fontWeight="bold" color="#900000">
         Ranking
       </Typography>
+
       <Slider
         step={1}
         color="warning"
         marks={ranks}
-        defaultValue={3}
+        value={filter.ranking}
         valueLabelDisplay="on"
-        getAriaValueText={valuetext}
-        aria-labelledby="label-always-visible-slider"
+        onChange={handleRankingChange}
         max={5}
         min={1}
       />
-      <div className=" border-b-2 border-[#D9D9D9] m-1" />
-      <Typography variant="h6" fontWeight={"bold"} color={"#900000"}>
+
+      <div className="border-b-2 border-[#D9D9D9] m-1" />
+
+      <Typography variant="h6" fontWeight="bold" color="#900000">
         Material
       </Typography>
-      <div>
+
+      <RadioGroup
+        aria-label="material"
+        name="material"
+        value={filter.material}
+        onChange={handleMaterialChange}
+      >
         <FormControlLabel
-          value="primary"
+          value="White Gold"
           control={<Radio color="error" />}
           label="White Gold"
         />
         <FormControlLabel
-          value="secondary"
+          value="Yellow Gold"
           control={<Radio color="error" />}
           label="Yellow Gold"
         />
         <FormControlLabel
-          value="success"
+          value="Rose Gold"
+          control={<Radio color="error" />}
           label="Rose Gold"
-          control={<Radio color="error" />}
         />
         <FormControlLabel
-          value="success"
+          value="Silver"
+          control={<Radio color="error" />}
           label="Silver"
-          control={<Radio color="error" />}
         />
         <FormControlLabel
-          value="success"
-          label="Sterling Silver"
+          value="Sterling Silver"
           control={<Radio color="error" />}
+          label="Sterling Silver"
         />
-        <FormControlLabel value="error" label="All" control={<Radio />} />
-      </div>
-      <div className=" border-b-2 border-[#D9D9D9] m-1" />
+        <FormControlLabel value="All" control={<Radio />} label="All" />
+      </RadioGroup>
+      <div className="border-b-2 border-[#D9D9D9] m-1" />
 
-      <Typography variant="h6" fontWeight={"bold"} color={"#900000"}>
+      <Typography variant="h6" fontWeight="bold" color="#900000">
         Made
       </Typography>
-      <div>
-        <FormControlLabel
-          value="success"
-          label="Handmade"
-          control={<Radio color="error" />}
-        />
-        <FormControlLabel
-          value="success"
-          label="Manufacture"
-          control={<Radio color="error" />}
-        />
-        <FormControlLabel value="error" label="All" control={<Radio />} />
-      </div>
-      <div className=" border-b-2 border-[#D9D9D9] m-1" />
 
-      <Typography variant="h6" fontWeight={"bold"} color={"#900000"}>
+      <RadioGroup
+        aria-label="made"
+        name="made"
+        value={filter.made}
+        onChange={handleMadeChange}
+      >
+        <FormControlLabel
+          value="Handmade"
+          control={<Radio color="error" />}
+          label="Handmade"
+        />
+        <FormControlLabel
+          value="Manufacture"
+          control={<Radio color="error" />}
+          label="Manufacture"
+        />
+        <FormControlLabel value="All" control={<Radio />} label="All" />
+      </RadioGroup>
+      <div className="border-b-2 border-[#D9D9D9] m-1" />
+
+      <Typography variant="h6" fontWeight="bold" color="#900000">
         Gender
       </Typography>
+
       <RadioGroup
         aria-label="gender"
         name="gender"
-        value={genderFilter}
-        onChange={(e) => setGenderFilter(e.target.value)}
+        value={filter.gender}
+        onChange={handleGenderChange}
       >
-        <div>
-          <FormControlLabel
-            value="male"
-            control={<Radio color="error" />}
-            label="Male"
-          />
-          <FormControlLabel
-            value="female"
-            control={<Radio color="error" />}
-            label="Female"
-          />
-          <FormControlLabel value="both" control={<Radio />} label="Both" />
-        </div>
+        <FormControlLabel
+          value="male"
+          control={<Radio color="error" />}
+          label="Male"
+        />
+        <FormControlLabel
+          value="female"
+          control={<Radio color="error" />}
+          label="Female"
+        />
+        <FormControlLabel value="both" control={<Radio />} label="Both" />
       </RadioGroup>
-      <div className=" border-b-2 border-[#D9D9D9] m-1" />
     </Drawer>
   );
 };
